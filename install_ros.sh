630becf1f853ff93f8c63c3d35a60dafcc7113b8
Kyungpyo Kim <37100596+Kyungpyo-Kim@users.noreply.github.com>
2020-12-22 11:01:52 +0900

    update ros env variables

--- install_ros.sh
+++ install_ros.sh
@@ -4,8 +4,13 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
 sudo apt install -y ros-melodic-desktop
+
 ## setup ros environment
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo source /opt/ros/melodic/setup.bash >> ~/.bashrc
+echo export ROS_MASTER_URI=http://localhost:11311 >> ~/.bashrc
+echo export ROS_IP=localhost >> ~/.bashrc
+echo export ROS_HOSTNAME=$ROS_IP >> ~/.bashrc
+
 source ~/.bashrc
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
